d2e27bfc4792c2be48a089646174909ced64e528
Alec Masterson <alecjmasterson@gmail.com>
2022-05-10 10:50:08 -0500

    add new types and interfaces

---
+++ src/types/iRow.ts
@@ -0,0 +1,11 @@
+export default interface iRow {
+    'Billing Account': string;
+    'Due Date': string;
+    'Invoice Date': string;
+    'Invoice Number': string;
+    'Product Name': string;
+    'Receivers Name'?: string;
+    'Sender Contact'?: string;
+    'Senders Name'?: string;
+    'Total Charge': number;
+}
